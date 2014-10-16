6dc8801540ffe295c07c031b16e47c4e0410d7d4
htian <htian@CIBMTR-182.mcwcorp.net>
2014-10-16 14:43:17 -0500

    push to github

---
+++ Tian-github/src/Tiangithub.java
@@ -0,0 +1,10 @@
+
+public class Tiangithub {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("This is my first try to link github. \n");
+
+	}
+
+}
