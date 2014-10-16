7bb6cca50453aeb094acd037d574e6b1e2035678
htian <htian@CIBMTR-182.mcwcorp.net>
2014-10-16 14:58:39 -0500

    3rd try

--- Tian-github/src/Tiangithub.java
+++ Tian-github/src/Tiangithub.java
@@ -5,7 +5,7 @@ public class Tiangithub {
 		// TODO Auto-generated method stub
 		System.out.println("This is my first try to link github. \n");
 		System.out.println("This is my second try to link github. \n");
-
+		System.out.println("This is my third try to link github. \n");
 	}
 
 }
